3aeeaed0c57fd6f903976390f3223c32dbfe1e10
devferx <ferkki@hotmail.com>
2021-08-13 22:33:03 -0400

    :tada: [ADD] initial files

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {Navigator} from './src/navigator/Navigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Icon name="star-outline" size={100} color="black" />
+    </View>
+  );
+};
